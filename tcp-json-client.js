41562dcdaaa4d30766abce995319144977078265
jian.wu <jian.wu@samsung.com>
2018-04-13 16:03:56 -0700

    simple node.js tcp json server with client, supporting two commands: continue and exit

--- tcp-json-client.js
+++ tcp-json-client.js
@@ -5,10 +5,15 @@ const HOST = '127.0.0.1';
 
 const client = new net.Socket();
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
 client.connect(PORT, HOST, function() {
     console.log('Connected');
     client.write(JSON.stringify({"message": "hello from client"}));
 });
+
 client.on('data', function(data) {
     let str_data = data.toString('utf8');
     let server_cmd = JSON.parse(str_data);
@@ -17,8 +22,11 @@ client.on('data', function(data) {
     
     if (server_cmd.cmd === 'exit') {
         client.destroy();
+    } else {
+        client.write(JSON.stringify({"message": "random message " + getRandomInt(3)}));
     }
 });
+
 client.on('close', function() {
     console.log('Connection closed');
 });
--- tcp-json-server.js
+++ tcp-json-server.js
@@ -5,7 +5,12 @@ const server = net.createServer();
 const PORT = 7070;
 const HOST = '127.0.0.1';
 
+const max_num_of_messages = 3;
+
+const continue_cmd = JSON.stringify({"cmd": "continue"});
 const exit_cmd = JSON.stringify({"cmd": "exit"});
+
+let message_cache = new Map();
 
 server.listen(PORT, HOST, () => {
     console.log('TCP Server is running on port ' + PORT);
@@ -14,17 +19,31 @@ server.listen(PORT, HOST, () => {
 server.on('connection', function(sock) {
     console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
 
+    message_cache.set(sock.remoteAddress, []);
+    
     // Add a 'data' event handler to this instance of socket
     sock.on('data', function(data) {
         // Convert ByteBuffer to UTF-8 String
         let str_data = data.toString('utf8');
+        let json_message = JSON.parse(str_data);
+        let message_bus = message_cache.get(sock.remoteAddress);
+        
         console.log('Received data from ' + sock.remoteAddress + ': ' + str_data);
         
-        sock.write(exit_cmd, 'utf8');
+        message_bus.push(json_message.message);
+        
+        if (message_bus.length >= 3) {
+            sock.write(exit_cmd, 'utf8');
+        } else {
+            sock.write(continue_cmd, 'utf8');
+        }
     });
     
     // Add a 'close' event handler to this instance of socket
     sock.on('close', function(data) {
         console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+        
+        message_bus = message_cache.get(sock.remoteAddress);
+        console.log("All the messages received from '" + sock.remoteAddress + "' are " + message_bus.toString());
     });
 });
